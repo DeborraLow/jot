90169cada2b49d3629d6cefa7395529d27153f73
Tommy DiLillo <tommydilillo@gmail.com>
2018-08-29 19:44:31 -0400

    saving, updated entriesService

---
+++ client/src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+
+import { EntryComponent } from './_Components/entry/entry.component';
+
+
+export const routes: Routes = [
+    { path: 'entry', component: EntryComponent },
+];
