5107804546940d9a62e0a8fa9b52903c1922f067
me@seanzamora.com <me@seanzamora.com>
2018-08-28 22:50:26 -0400

    Setting up codebase structure.

--- client/src/app/_Models/User.ts
+++ client/src/app/_Models/User.ts
@@ -1,12 +1,12 @@
-interface User{
-    id:string;
-    pen_name:string;
-    avatar:string;
+interface User {
+    id: string;
+    pen_name: string;
+    avatar: string;
 }
 
-interface UserSettings{
-    email:string;
-    username:string;
-    first_name:string;
-    last_name:string;
+interface UserSettings {
+    email: string;
+    username: string;
+    first_name: string;
+    last_name: string;
 }
